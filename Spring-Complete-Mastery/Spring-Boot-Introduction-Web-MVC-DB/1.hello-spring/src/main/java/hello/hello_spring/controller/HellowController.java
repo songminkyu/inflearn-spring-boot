8051116d2d204cd528ae893e0fd79189000721a8
songminkyu <rabbircarrot@naver.com>
2025-04-19 15:36:06 +0900

    add : mvc 컨트롤 샘플 추가

--- Spring-Complete-Mastery/Spring-Boot-Introduction-Web-MVC-DB/1.hello-spring/src/main/java/hello/hello_spring/controller/HellowController.java
+++ Spring-Complete-Mastery/Spring-Boot-Introduction-Web-MVC-DB/1.hello-spring/src/main/java/hello/hello_spring/controller/HellowController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HellowController {
@@ -10,5 +11,11 @@ public class HellowController {
     public String hello(Model model) {
         model.addAttribute("data", "Hello Spring!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
