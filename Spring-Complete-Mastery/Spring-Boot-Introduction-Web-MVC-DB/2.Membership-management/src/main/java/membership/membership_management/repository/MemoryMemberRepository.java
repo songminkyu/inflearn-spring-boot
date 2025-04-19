9deaa2041338f9d39ad4d6e82c23f51b1fc6d1d7
songminkyu <rabbircarrot@naver.com>
2025-04-19 16:35:13 +0900

    add : 회원관리 예제 추가

---
+++ Spring-Complete-Mastery/Spring-Boot-Introduction-Web-MVC-DB/2.Membership-management/src/main/java/membership/membership_management/repository/MemoryMemberRepository.java
@@ -0,0 +1,33 @@
+package membership.membership_management.repository;
+
+import membership.membership_management.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return store.containsKey(id) ? Optional.ofNullable(store.get(id)) : Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
