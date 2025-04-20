23e38ccf8b92b9d2b1126d71f86db5c4d4258ca2
songminkyu <rabbircarrot@naver.com>
2025-04-20 19:02:44 +0900

    1. 회원 웹기능-조회 예제 코드 추가 2. 코드 수정 시 자동 빌드 반영 적용

---
+++ Spring-Complete-Mastery/Spring-Boot-Introduction-Web-MVC-DB/2.Membership-management/src/main/java/membership/membership_management/controller/HomeController.java
@@ -0,0 +1,13 @@
+package membership.membership_management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
--- Spring-Complete-Mastery/Spring-Boot-Introduction-Web-MVC-DB/2.Membership-management/src/main/java/membership/membership_management/controller/MemberController.java
+++ Spring-Complete-Mastery/Spring-Boot-Introduction-Web-MVC-DB/2.Membership-management/src/main/java/membership/membership_management/controller/MemberController.java
@@ -1,8 +1,14 @@
 package membership.membership_management.controller;
 
+import membership.membership_management.domain.Member;
 import membership.membership_management.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
