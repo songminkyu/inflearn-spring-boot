d9fbd2a9afb77dd000291aa9384684c813a5a26a
songminkyu <rabbircarrot@naver.com>
2025-04-21 20:00:09 +0900

    add : AOP 예제 적용

---
+++ Spring-Complete-Mastery/Spring-Boot-Introduction-Web-MVC-DB/3.Membership-management-JPA/src/main/java/membership/membership_management/aop/TimeTraceAop.java
@@ -0,0 +1,23 @@
+package membership.membership_management.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+    @Around("execution(* membership.membership_management..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString()+ " " + timeMs + "ms");
+        }
+    }
+}
