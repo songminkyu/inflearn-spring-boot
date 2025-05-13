b0ed0a8f46337b2cec3b20d2b7863fb42fb531e2
songminkyu <rabbircarrot@naver.com>
2025-05-13 15:33:27 +0900

    Add servlets to handle HTTP requests and JSON parsing
    Introduced new servlets to process request headers, parameters, raw strings, and JSON payloads. Included a form HTML for testing parameter submission and a HelloData class with Lombok annotations for data binding. These changes enable comprehensive handling of HTTP request data in various formats.

---
+++ Spring-Complete-Mastery/Spring-Boot-MVC-1/servlet/src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,23 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import org.springframework.util.StreamUtils;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name="requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(jakarta.servlet.http.HttpServletRequest request,
+                           jakarta.servlet.http.HttpServletResponse response)
+    throws ServletException, java.io.IOException
+    {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+        response.getWriter().write("ok");
+    }
+}
